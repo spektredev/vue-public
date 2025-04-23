ece7255f2a5c8a81cdf8dbb0473be0b33437ad8a
Eugene <arcseostd@gmail.com>
2025-04-23 23:21:50 +0300

    testing ci\cd, test commit

--- mocks/categories.ts
+++ mocks/categories.ts
@@ -8,3 +8,4 @@ export const mockCategories: Category[] = [
   { id: 1, title: 'Новости', description: '', link: 'news', cat_image: '/images/categories/news.png' },
   { id: 1, title: 'Новости', description: '', link: 'news', cat_image: '/images/categories/news.png' },
 ];
+// testing ci\cd, test commit
